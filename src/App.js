c478548d0e4908bfc7776d0fef1a9e929184cb00
Mohamed Hatem <mohamedhatemakl@gmail.com>
2021-07-07 00:16:34 +0200

    complete trending page and pagenation

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav/MainNav";
+import Trending from "./pages/Trending/Trending";
+import Movies from "./pages/Movies/Movies";
+import Series from "./pages/Series/Series";
+import Search from "./pages/Search/Search";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
+  );
+}
+
+export default App;
